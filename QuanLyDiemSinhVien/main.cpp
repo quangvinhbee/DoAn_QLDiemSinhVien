2a96a2c7a367146637c12c627c8ad8a0cb7b92fe
cuongnguyen1002 <cuongnguyena003@gmail.com>
2022-06-20 11:51:03 +0700

    first commit

---
+++ QuanLyDiemSinhVien/main.cpp
@@ -0,0 +1,46 @@
+#include "MonHoc.h"
+#include "Xuly.h"
+#include "LopTinChi.h"
+#include "SinhVien.h"
+#include<iostream>
+#include <typeinfo>
+#include "DiemMonHocCuaLop.h"
+using namespace std;
+#include <Windows.h>
+
+
+int main()
+{
+
+	COORD coord;
+	coord.X = 1500;
+	coord.Y = 400;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	//fullscreen();
+	system("color 5A");
+	//ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+	SetConsoleScreenBufferSize(handle, coord);
+	PTR_LISTCREDITCLASS listCC = new LIST_CREDITCLASS;
+	TREE_SUBJECT t;
+	LIST_STUDENT l;
+
+	InitTreeSubject(t);
+	InitListStudent(l);
+
+	LoadCreditClassFromFile(listCC);
+	LoadSubjectFormFile(t);
+	LoadStudentFromFile(l);
+
+	MergeAll(listCC, t, l);
+
+	SaveCreditClassToFile(listCC);
+	SaveStudentToFile(l);
+	SaveSubjectToFile(t);
+	//	string a = "          ";
+	//	trim(a);
+	//	StandarString(a);
+	//	cout << "1"+a+"1";
+	return 0;
+}
+
+
